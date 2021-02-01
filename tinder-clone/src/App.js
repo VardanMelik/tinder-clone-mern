e317266668fc57c325a5def519fefdf2597c7d1e
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-01 12:06:16 +0400

    update

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import TinderCards from './components/TinderCards';
+import SwipeButtons from './components/SwipeButtons';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Header/>
       <TinderCards/>
+      <SwipeButtons/>
     </div>
   );
 }
--- tinder-clone/src/components/Header.js
+++ tinder-clone/src/components/Header.js
@@ -5,6 +5,7 @@ import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import TinderLogo from '../assets/tinder-logo.png';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
@@ -12,11 +13,13 @@ function Header() {
             <IconButton>
                 <PersonIcon className="header__icon"/>
             </IconButton>
-            <img 
-                className="header__logo"
-                src={TinderLogo}
-                alt=""
-            />
+            <Link to="/">
+                <img 
+                    className="header__logo"
+                    src={TinderLogo}
+                    alt=""
+                />
+            </Link>
             <IconButton>
                 <QuestionAnswerIcon className="header__icon"/>
             </IconButton>
