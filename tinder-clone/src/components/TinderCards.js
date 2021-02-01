2a4f1bab0a86f77a5edfb069be9ccbedcf379f3e
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-01 11:41:25 +0400

    update

--- tinder-clone/src/components/TinderCards.js
+++ tinder-clone/src/components/TinderCards.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TinderCards.css';
 import TinderCard from 'react-tinder-card';
+import { SwipeableDrawer } from '@material-ui/core';
 
 function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: 'Elon Musk',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg'
+        },
+        {
+            name: 'Jeff Bezoz',
+            url: 'https://www.gstatic.com/tv/thumb/persons/532529/532529_v9_bb.jpg'
+        }
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+        //setLastDirection(direction);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + "left the screen!");
+    }
+
     return (
         <div className="tinderCards">
-            Hello
+            <div className="tinderCards__cardContainer">
+                {people.map( (person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={ () => outOfFrame(person.name)}
+                    >
+                    <div 
+                        style={{backgroundImage: `url(${person.url})` }}
+                        className="card"
+                        >
+                        <h3>{person.name}</h3>
+                    </div>
+                    
+                    </TinderCard>
+                ))}
+            </div>
+            
         </div>
     )
 }
